1dc54c3022347085952c377a0293081b25750fe9
Amos Tech <103753361+A-Tech-Services@users.noreply.github.com>
2023-07-26 18:05:01 +0100

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,35 +5,48 @@ import "./HomePageStyles.css"
 
 const HomePage = () => {
     const [coinNewsDatas, setCoinNews] = useState([]);
+    const [page, setPage] = useState(1);
     
     
-    const options = {
-        method: 'GET',
-        url: 'https://investing-cryptocurrency-markets.p.rapidapi.com/coins/get-news',
-        params: {
-          pair_ID: '1057391',
-          page: '1',
-          time_utc_offset: '28800',
-          lang_ID: '1'
-        },
-        headers: {
-          'X-RapidAPI-Key': 'c543991eccmsha9d03b5659fa628p18c9ffjsn98994b37dc17',
-          'X-RapidAPI-Host': 'investing-cryptocurrency-markets.p.rapidapi.com'
-        }
-    };
 
     // handling our response.
     useEffect(() => {
+        const options = {
+            method: 'GET',
+            url: 'https://investing-cryptocurrency-markets.p.rapidapi.com/coins/get-news',
+            params: {
+              pair_ID: '1057391',
+              page: page,
+              time_utc_offset: '28800',
+              lang_ID: '1'
+            },
+            headers: {
+              'X-RapidAPI-Key': 'c543991eccmsha9d03b5659fa628p18c9ffjsn98994b37dc17',
+              'X-RapidAPI-Host': 'investing-cryptocurrency-markets.p.rapidapi.com'
+            }
+        };
+
+        
         axios.request(options)
         .then((response) => {
             console.log(response.data);
             setCoinNews(response.data.data[0].screen_data.news);
         })
         .catch((err) => console.log(err.message));
-    }, [])
+    }, [page])
 
 
     const tagRegExp =  new RegExp('<\s*[^>]*>', 'g');
+
+    const nextPage = () => {
+            let newValue = page + 1;
+            setPage(newValue);
+        }
+
+    const previousPage = () => {
+        let newValue = page - 1;
+        setPage(newValue);
+    }
 
   return (
 
@@ -56,8 +69,8 @@ const HomePage = () => {
         }
 
         <div className="btns">
-            <button >Previous Page</button>
-            <button >Next Page</button>
+            <button onClick={previousPage}>Previous Page</button>
+            <button onClick={nextPage}>Next Page</button>
         </div>
         </div>
     </div>
